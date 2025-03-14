a78d72386066de578419032f31323029969a67ac
vanlandschootsh900 <vanlandschootsh900@northwested.org>
2025-03-14 10:22:33 -0400

    s

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 import pygame
 import sys
 import config # Import the config module
+import random
+
 
 def init_game ():
 
@@ -14,6 +16,18 @@ def init_game ():
     
     pygame.display.set_caption(config.TITLE)
     return screen
+
+def draw_text(screen, text, x, y, font_size, color, font_name=None, bold=False, italic=False):
+    if font_name:
+        font=pygame.font.font(font_name, font_size)
+    else:
+        font = pygame.font.Font(None, font_size)
+
+    font.set_bold(bold)
+    font.set_italic(italic)
+
+    text_surface = font.render(text, True, color)
+    screen.blit(text_surface,(x,y)) 
 
 def handle_events ():
     for event in pygame.event.get():
@@ -34,16 +48,28 @@ def main():
     running = True
     while running:
         running = handle_events()
-        screen.fill(config.GRAY) # Use color from config
+        screen.fill(config.WHITE) # Use color from config
         
         # Add code to draw stuff (for example) below this comment
 
+        text1= 'Shay VanLandschoot'
+        font_size1 = 48
+        color1 = config.BLACK
+        x1, y1 = (200,250)
 
+        text2= 'Am Web and app'
+        font_size2 = 48
+        color2 = config.RED
+        x2, y2 = (200,320)
 
+        text3= 'SuttonBay Online'
+        font_size3 = 48
+        color3 = config.PURPLE
+        x3, y3 = (475,355)
 
-
-
-
+        draw_text(screen, text1 ,x1 , y1, font_size1, color1)
+        draw_text(screen, text2 ,x2 , y2, font_size2, color2, bold=True)
+        draw_text(screen, text3 ,x3 , y3, font_size3, color3, italic=True)
 
 
         pygame.display.flip()
